92e1813fd296784a6b0e73ead9345e3e217d9ae3
Chaua Queirolo <chaua@users.noreply.github.com>
2018-05-22 19:09:58 -0300

    Import inicial

---
+++ exemplos/App04BrothersNetworks/app/src/main/java/br/com/utp/brothersnetworks/MainActivity.java
@@ -0,0 +1,13 @@
+package br.com.utp.brothersnetworks;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ exemplos/App04BrothersNetworks/app/src/main/java/br/com/utp/brothersnetworks/activities/LoginActivity.java
@@ -0,0 +1,12 @@
+package br.com.utp.brothersnetworks.activities;
+
+import android.support.v7.app.AppCompatActivity;
+
+import org.androidannotations.annotations.EActivity;
+
+import br.com.utp.brothersnetworks.R;
+
+@EActivity(R.layout.activity_main)
+public class LoginActivity extends AppCompatActivity {
+
+}
